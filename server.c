824970c211e7ac511708f96d9c0a23b24031abd4
“Song-lz” <1643319918@qq.com>
2021-08-25 23:07:38 +0800

    20210825

--- server.c
+++ server.c
@@ -4,10 +4,13 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include <string.h>
+#include <pthread.h>
+
 #define PORT 			7896
 #define SELECT_HANDLERS   16
-
+#define LISTEN_NUM_MAX    10
 #define SELECT_NFDS_UPDATE(__obj) \
 {\
 	unsigned char __i; \
@@ -35,31 +38,37 @@ typedef struct selectObjST {
 	SelectHandlerST *handlers[SELECT_HANDLERS];
 } selectObjST;
 
+int tcpListen(int port);
+
+void *pthread_socket(void *arg);
+
 
 int main(int argc, char **argv)
 {
-
-	tcpListen();
+	printf("1\r\n");
+	sleep(1);
+	pthread_t pthread_select;
+	printf("2\r\n");
+	tcpListen(PORT);
+	printf("3\r\n");
 
 	
+	pthread_create(&pthread_select, NULL, pthread_socket, NULL);
+	printf("4\r\n");
+
+	pthread_join(pthread_select, NULL);
+
+
+
 
 	return 0;
 }
-
-
-int tcpListen(char *ip, int port)
+void selectLoop(void);
+void *pthread_socket(void *arg)
 {
-	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(socketfd < 0){
-		perror("socket err");
+	while(1){
+		selectLoop();
 	}
-	struct sockaddr_in addr;
-	/* 本机字节顺序转化为网络字节顺序,小端转大端 */
-	addr.sin_port = htonl(PORT);
-	addr.sin_addr.s_addr = htons(INADDR_ANY);
-	addr.sin_family = AF_INET;
-	bind(socketfd, (struct sockaddr *)&addr, (socklen_t )sizeof(struct sockaddr_in));
-	
 }
 
 selectObjST *selectObjGet(void)
@@ -117,19 +126,23 @@ int selectAdd(int fd, void(*recvcb)(void *), void *data)
 		if(!obj->handlers[i])
 		{index = i;}
 	}
+	printf("%s %d\r\n", __func__, __LINE__);
 
 	if(index == -1)
 	{return -1;}
+	printf("%s %d\r\n", __func__, __LINE__);
 
 	obj->handlers[i] = (SelectHandlerST*)malloc(sizeof(SelectHandlerST));
+	printf("%s %d\r\n", __func__, __LINE__);
 	if(!obj->handlers[i])
 	{return -1;}
-	obj->handlers[i]->activeflag = true;
 	obj->handlers[i]->fd = fd;
+	printf("%s %d\r\n", __func__, __LINE__);
 	obj->handlers[i]->cb = recvcb;
+	printf("%s %d\r\n", __func__, __LINE__);
 	obj->handlers[i]->data = data;
 	SELECT_NFDS_UPDATE(obj);
-	
+	printf("%s %d\r\n", __func__, __LINE__);
 }
 
 int selectDel(int fd)
@@ -177,4 +190,47 @@ void selectExit(void)
 	
 }
 
+
+
+void peer_client(void *arg)
+{
+	char buf[50]={0};
+	ssize_t err = read((int)arg, buf, sizeof(buf));
+	if(err == 0){
+		selectDel((int)arg);
+	}
+}
+void host_listen(void *arg)
+{
+	struct sockaddr addr;
+	socklen_t len;
+	int clientfd =  accept((int)arg, &addr, &len);
+	if(clientfd < 0){
+		perror("accept err");
+		return;
+	}
+	selectAdd(clientfd, peer_client, (void *)clientfd);
+}
+
+
+int tcpListen(int port)
+{
+	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socketfd < 0){
+		perror("socket err");
+		return -1;
+	}
+	struct sockaddr_in addr;
+	/* 本机字节顺序转化为网络字节顺序,小端转大端 */
+	addr.sin_port = htonl(PORT);
+	addr.sin_addr.s_addr = htons(INADDR_ANY);
+	addr.sin_family = AF_INET;
+	printf("%s %d\r\n", __func__, __LINE__);
+	bind(socketfd, (struct sockaddr *)&addr, (socklen_t )sizeof(struct sockaddr_in));
+	printf("%s %d\r\n", __func__, __LINE__);
+	listen(socketfd, LISTEN_NUM_MAX);	
+	printf("%s %d\r\n", __func__, __LINE__);
+	selectAdd(socketfd, host_listen, (void *)socketfd);
+	printf("%s %d\r\n", __func__, __LINE__);
+}
 
